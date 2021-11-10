360646cb38a68d83e6c5049fe03cc6089fb1e0bf
RafaelOsiro <rafaelosiro@hotmail.com>
2021-11-09 21:46:23 -0300

    First init of the project

---
+++ E1.c
@@ -0,0 +1,19 @@
+/*
+UNIVERSIDADE CATÓLICA DE BRASÍLIA
+CURSO: ANÁLISE E DESENVOLVIMENTO DE SISTEMAS
+DISCIPLINA: PROGRAMAÇÃO ESTRUTURADA
+DOCENTE: JOYCE SIQUEIRA
+DISCENTE: RAFAEL RIKI OGAWA OSIRO (UC21100716)
+
+Exercício: 1. Leia o nome completo de 2 pessoas e apresente o primeiro nome somente em letras maiúsculas e na 
+linha seguinte, com uma tabulação, o segundo nome em letras minúsculas, solicitando outros nomes caso o usuário
+ deseje. Somente devem ser usadas funções do padrão ANSI da linguagem C.
+*/
+#include<stdio.h>
+
+int main()
+{
+
+    printf("Debug");
+    return 0;
+}
